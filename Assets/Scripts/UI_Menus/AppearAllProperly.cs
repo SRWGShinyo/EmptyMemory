fde06c467a87245322c419189506fc445bedd985
Yohann Degli Esposti <yohann.degliesposti@gmail.com>
2021-01-31 13:06:51 +0100

    main menu: is now done and functionnal.

---
+++ Assets/Scripts/UI_Menus/AppearAllProperly.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearAllProperly : MonoBehaviour
+{
+
+    public List<CanvasGroup> groups = new List<CanvasGroup>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(appearAll());
+    }
+
+    private IEnumerator appearAll()
+    {
+        foreach (CanvasGroup cg in groups)
+        {
+            while (cg.alpha < 1)
+            {
+                cg.alpha += 0.01f;
+                yield return new WaitForSeconds(0.01f);
+            }
+            yield return new WaitForSeconds(0.6f);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/UI_Menus/BasicButtonFunctions.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class BasicButtonFunctions : MonoBehaviour
+{
+    public CanvasGroup panel;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Open()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        panel.blocksRaycasts = true;
+        panel.interactable = true;
+
+        StartCoroutine(Appear(true));
+    }
+
+    public void Close()
+    {
+        panel.blocksRaycasts = false;
+        panel.interactable = false;
+
+        StartCoroutine(Appear(false));
+    }
+
+    private IEnumerator Appear(bool appaerOrnot)
+    {
+        if (appaerOrnot)
+        {
+            while (panel.alpha < 1)
+            {
+                panel.alpha += 0.01f;
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+
+        else
+        {
+            while (panel.alpha > 0)
+            {
+                panel.alpha -= 0.01f;
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
