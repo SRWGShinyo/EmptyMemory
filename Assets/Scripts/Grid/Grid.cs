35e0603b971ae685241ae488056dd417afb1aecd
Yohann Degli Esposti <yohann.degliesposti@gmail.com>
2021-01-31 11:03:45 +0100

    init commit

---
+++ Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public static Grid grid;
+
+    int[,] grid_matrix = new int[5,5] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 },
+                                        { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, 
+                                        { 0, 0, 0, 0, 0 } };
+
+    private void Awake()
+    {
+        if (grid == null)
+            grid = this;
+        else
+            Destroy(grid.gameObject);
+    }
+
+
+    public void SnapPieceToGrid(GridCell cell, PositionPiece piece)
+    {
+
+        List<(int, int)> positionsToOccupy = new List<(int, int)>();
+        (int, int) positions = (cell.line, cell.column);
+        for (int i = 0; i < piece.matrix.Count; i++)
+        {
+            positions.Item1 += i;
+            for (int j = 0; j < piece.matrix[i].column.Count; j++)
+            {
+                positions.Item2 += j;
+                if (positions.Item1 >= 5 || positions.Item2 >= 5 || grid_matrix[positions.Item1, positions.Item2] == 1)
+                {
+                    Debug.Log(grid_matrix[positions.Item1, positions.Item2]);
+                    Debug.Log("Cell is nope " + positions.Item1 + " " + positions.Item2);
+                    return;
+                }
+
+                Debug.Log(" " + positions + " " + i + " " + j);
+                positionsToOccupy.Add((positions.Item1, positions.Item2));
+            }
+
+
+            positions.Item2 = cell.column;
+        }
+
+        piece.isSnapped = false;
+        piece.transform.position = new Vector3(cell.transform.position.x, cell.transform.position.y, piece.transform.position.z);
+        piece.snappedTo = cell;
+        SetPositionsToGrid(positionsToOccupy, 1);
+        piece.positions = positionsToOccupy;
+
+        Debug.Log("Piece is snappedTo" + positionsToOccupy[0] + positionsToOccupy[1]);
+    }
+
+    public void unSnapToGrid(PositionPiece piece, List<(int, int)> positions)
+    {
+        piece.snappedTo = null;
+        piece.positions = null;
+        SetPositionsToGrid(positions, 0);
+    }
+
+    public void SetPositionsToGrid(List<(int, int)> positions, int value)
+    {
+        foreach ((int, int) pos in positions)
+            grid_matrix[pos.Item1, pos.Item2] = value;
+    }
+}
---
+++ Assets/Scripts/Grid/PositionPiece.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionPiece : MonoBehaviour
+{
+    [System.Serializable]
+    public class VectorStored
+    {
+        public List<int> column;
+    }
+
+    public List<SpriteRenderer> graphics;
+    public List<VectorStored> matrix = new List<VectorStored>();
+    public List<(int, int)> positions = new List<(int, int)>();
+
+    public GridCell snappedTo;
+    public bool isSnapped;
+
+    bool selected;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSnapped)
+        {
+            Vector2 mousePOsition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mousePOsition.x, mousePOsition.y, transform.position.z);
+        }
+
+        if (Input.GetMouseButtonDown(0) && selected)
+        {
+            if (!snappedTo)
+                isSnapped = true;
+            else
+            {
+                Debug.Log("Snap1");
+                isSnapped = true;
+                Grid.grid.unSnapToGrid(this, positions);
+            }
+        }
+
+        else if (Input.GetMouseButtonDown(1) && isSnapped)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, LayerMask.GetMask("Cell"));
+            if (hit.collider != null && hit.transform.GetComponent<GridCell>())
+            {
+                Debug.Log("Snap2");
+                Grid.grid.SnapPieceToGrid(hit.transform.GetComponent<GridCell>(), this);
+            }
+
+            else
+            {
+                isSnapped = false;
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        selected = true;
+        foreach (SpriteRenderer s in graphics)
+            s.color = Color.blue;
+    }
+
+    private void OnMouseExit()
+    {
+        selected = false;
+        foreach (SpriteRenderer s in graphics)
+            s.color = Color.red;
+    }
+}
